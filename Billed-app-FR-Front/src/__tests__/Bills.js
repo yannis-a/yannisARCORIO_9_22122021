48ed1e66b0ec7eb8b34c8db82d5ef0338d304d6d
Yannis <arcorio.y@gmail.com>
2021-12-30 11:01:11 +0100

    test bills

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -5,10 +5,13 @@ import { bills } from "../fixtures/bills.js"
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", () => {
-      const html = BillsUI({ data: [] })
+      const html = BillsUI({ data: bills })
       document.body.innerHTML = html
-      //to-do write expect expression
+      const icon = screen.getByTestId('icon-window')
+      const classList = icon.classList
+      expect(classList.contains('active-icon')).toBe(true)
     })
+
     test("Then bills should be ordered from earliest to latest", () => {
       const html = BillsUI({ data: bills })
       document.body.innerHTML = html
