f966e22078e495733b1c0785e072a491b29809fd
Yannis <arcorio.y@gmail.com>
2022-02-02 22:26:46 +0100

    test new bills

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -1,14 +1,90 @@
-import { screen } from "@testing-library/dom"
+import { fireEvent, screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
+import store from "../__mocks__/store.js"
 
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
+    test("Then the newBill page should be rendered", () => {
       const html = NewBillUI()
       document.body.innerHTML = html
-      //to-do write assertion
+      const newBillElement = screen.getAllByText("Envoyer une note de frais")
+      expect(newBillElement).toBeTruthy()
+    })
+    test("Then a form with nine fields should be rendered", () => {
+      document.body.innerHTML = NewBillUI()
+      const form = document.querySelector("form")
+      expect(form.length).toEqual(9)
+    })
+  })
+  describe("And I upload a image file", () => {
+    test("Then the file handler should show a file", () => {
+      localStorage.setItem('user', JSON.stringify({ type: 'Employee', email: 'employee@test.tld' }))
+      document.body.innerHTML = NewBillUI()
+      const newBill = new NewBill({ document, onNavigate: () => { return }, store: store, localStorage: window.localStorage })
+      const handleChangeFile = jest.fn(() => newBill.handleChangeFile)
+      const inputFile = screen.getByTestId("file")
+      inputFile.addEventListener("change", handleChangeFile)
+      fireEvent.change(inputFile, {
+        target: {
+          files: [new File(["image.jpg"], "image.jpg", { type: "image/jpg" })],
+        }
+      })
+      const numberOfFile = screen.getByTestId("file").files.length
+      expect(numberOfFile).toEqual(1)
+    })
+  })
+  describe("And I upload a non-image file", () => {
+    test("Then the error message should be display", async () => {
+      localStorage.setItem('user', JSON.stringify({ type: 'Employee', email: 'employee@test.tld' }))
+      document.body.innerHTML = NewBillUI()
+      const newBill = new NewBill({ document, onNavigate: () => { return }, store: store, localStorage: window.localStorage })
+      const handleChangeFile = jest.fn(() => newBill.handleChangeFile)
+      const alert = global.alert = jest.fn()
+      const inputFile = screen.getByTestId("file")
+      inputFile.addEventListener("change", handleChangeFile)
+      fireEvent.change(inputFile, {
+          target: {
+              files: [new File(["sample.txt"], "sample.txt", { type: "text/txt" })],
+          }
+      })
+      expect(handleChangeFile).toBeCalled()
+      expect(inputFile.files[0].name).toBe("sample.txt")
+      expect(alert).toBeCalled()
+    })
+  })
+
+  describe("And I submit a valid bill form", () => {
+    test('then a bill is created', async () => {
+      localStorage.setItem('user', JSON.stringify({ type: 'Employee', email: 'employee@test.tld' }))
+      document.body.innerHTML = NewBillUI()
+      const newBill = new NewBill({ document, onNavigate: () => { return }, store: store, localStorage: window.localStorage })
+      const form = screen.getByTestId('form-new-bill')
+      const validBill = {
+        name: "validBill",
+        date: "2021-01-01",
+        type: "Restaurants et bars",
+        amount: 10,
+        pct: 10,
+        vat: "40",
+        fileName: "test.jpg",
+        fileUrl: "https://test/test.jpg"
+      }
+      const handleSubmit = jest.fn((e) => newBill.handleSubmit(e))
+      newBill.updateBill = (newBill) => newBill
+      document.querySelector(`input[data-testid="expense-name"]`).value = validBill.name
+      document.querySelector(`input[data-testid="datepicker"]`).value = validBill.date
+      document.querySelector(`select[data-testid="expense-type"]`).value = validBill.type
+      document.querySelector(`input[data-testid="amount"]`).value = validBill.amount
+      document.querySelector(`input[data-testid="vat"]`).value = validBill.vat
+      document.querySelector(`input[data-testid="pct"]`).value = validBill.pct
+      document.querySelector(`textarea[data-testid="commentary"]`).value = validBill.commentary
+      newBill.fileUrl = validBill.fileUrl
+      newBill.fileName = validBill.fileName 
+      form.addEventListener('click', handleSubmit)
+      fireEvent.click(form)
+      expect(handleSubmit).toHaveBeenCalled()
     })
   })
 })
